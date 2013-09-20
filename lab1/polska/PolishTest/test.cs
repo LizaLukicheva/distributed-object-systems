b73533b8397fcaa8f4f992fb6d73d5690ab7ba93
LizaLukicheva <vocaloidvnuchka@gmail.com>
2013-09-20 19:31:02 +0600

    Polish string: unnesessary strings deleted

--- lab1/polska/PolishTest/test.cs
+++ lab1/polska/PolishTest/test.cs
@@ -14,10 +14,16 @@ namespace PolishTest
         public test()
         {
             testStrings.Add("10+(15-5)*2", "10 15 5 - 2 * +");
-            testStrings.Add("74+(5-2)*4","74 5 2 - 4 * +");
+            testStrings.Add("74+(5-2)*4", "74 5 2 - 4 * +");
+            testStrings.Add("3+4", "3 4 +");
+            testStrings.Add("7-2*3", "7 2 3 * -");
+            testStrings.Add("2*3-7", "2 3 * 7 -");
 
             calculatedResults.Add(30);
             calculatedResults.Add(86);
+            calculatedResults.Add(7);
+            calculatedResults.Add(1);
+            calculatedResults.Add(-1);
         }
 
         [TestMethod]
@@ -26,7 +32,7 @@ namespace PolishTest
             foreach(var testPair in testStrings)
             {
                 var p = new PolishString(testPair.Key);
-                Assert.AreEqual(p.getPolska(), testPair.Value);
+                Assert.AreEqual(testPair.Value, p.getPolska());
             }
         }
 
@@ -34,10 +40,19 @@ namespace PolishTest
         public void TestCalculateMethod()
         {
             var p = new PolishString("10+(15-5)*2");
-            Assert.AreEqual(p.calculatePolska(), 30);
+            Assert.AreEqual(30, p.calculatePolska());
 
             p = new PolishString("74+(5-2)*4");
-            Assert.AreEqual(p.calculatePolska(), 86);
+            Assert.AreEqual(86, p.calculatePolska());
+
+            p = new PolishString("3+4");
+            Assert.AreEqual(7, p.calculatePolska());
+
+            p = new PolishString("7-2*3");
+            Assert.AreEqual(1, p.calculatePolska());
+
+            p = new PolishString("2*3-7");
+            Assert.AreEqual(-1, p.calculatePolska());
         }
     }
 }
---
+++ lab1/polska/polska/PolishCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polska
+{
+    class PolishCalculator
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                System.Console.Write("Enter expression: ");
+                string readedString = System.Console.ReadLine();
+                var p = new PolishString(readedString);
+                System.Console.WriteLine("Polish expression: ", p.getPolska() + " = " + Convert.ToString(p.calculatePolska()));
+            }
+        }
+    }
+}
